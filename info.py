660e95cce1ef1e70cd22b8371c38795abe96e856
S3HID N3W3B <166154843+NBOTZ001@users.noreply.github.com>
2025-05-04 12:11:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24363060'))
 API_HASH = environ.get('API_HASH', '2403084c19a00e8f07874b15ba01557b')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6746522771:AAFQJ6Lmb6nWZXlGeUthzagIf4chXNTVLVo")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7115462301:AAE15Z8BDGlPuOR2N-cGl4szrXYu-Xh83XE")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -50,7 +50,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nawab001:nawab001@cluster0.uiddjn8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://o0td73:ZBAcnuBGqhLpxQbJ@cluster0.vqiboka.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "thenawab")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
