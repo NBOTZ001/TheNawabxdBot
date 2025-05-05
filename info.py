a9d1c059ccc7a079c08e4d4e9d8335e405ed570e
S3HID N3W3B <166154843+NBOTZ001@users.noreply.github.com>
2025-05-05 12:06:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24363060'))
 API_HASH = environ.get('API_HASH', '2403084c19a00e8f07874b15ba01557b')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7115462301:AAE15Z8BDGlPuOR2N-cGl4szrXYu-Xh83XE")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7115462301:AAGFqxBbw7H6VWqhWCtgrFB-PU7OtkARVGY")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
